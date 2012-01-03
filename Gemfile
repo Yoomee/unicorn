85cc2266b1290f4ba81850c6c716b7cd7e852869
Matt Atkins <matt@yoomee.com>
2012-01-03 10:51:38 +0000

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+gem 'haml'
+gem 'xml-simple'
+gem 'json'
+gem 'jquery-rails'
+gem 'will_paginate'
+
+group :production do
+  gem 'pg'
+end
+
+group :development do
+  gem 'mysql2'
+  gem 'sqlite3-ruby'
+end
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amnesty::Application.config.secret_token = '66d7e5f5bb3c84a0f53f97bbda2079f328a48e067b76a95c66ef38c400459baf8955909e156b3f991906fe3d70bc60f70892cdfc4d0695d00d026ba8a029181f'
