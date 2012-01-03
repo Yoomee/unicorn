4d9632b82b73356f66b8cb67334098fa8ed95ddd
Matt Atkins <matt@yoomee.com>
2012-01-03 12:53:44 +0000

    Rough home page layout with share links.

---
+++ app/helpers/share_helper.rb
@@ -0,0 +1,47 @@
+module ShareHelper
+  
+  # Supporting javascripts
+  def facebook_js
+    fb_js = <<-JAVASCRIPT
+    (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    JAVASCRIPT
+    javascript_tag(fb_js)
+  end
+  
+  def google_js
+    g_js = <<-JAVASCRIPT
+      window.___gcfg = {lang: 'en-GB'};
+      (function() {
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+      })();
+    JAVASCRIPT
+    javascript_tag(g_js)
+  end
+  
+  def sharing_js
+    google_js + facebook_js
+  end
+  
+  # Buttons
+  def facebook_share 
+    content_tag(:div, nil, :class => 'fb-like', :"data-send" => false, :"data-layout" => "button_count", :"data-width" => "90", :"data-show-faces" => false)
+  end
+  
+  def google_share
+    content_tag(:div, nil, :class => 'g-plusone', :"data-size" => "medium")
+  end
+  
+  def twitter_share
+    out = link_to("Tweet", "https://twitter.com/share", :class => "twitter-share-button", :"data-text" => "Check out this site", :"data-related" => "amnesty")
+    out << javascript_tag("!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"//platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");")
+  end
+
+end
