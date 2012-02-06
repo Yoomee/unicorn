3b3ad62efde53eb888fdeb23561dbd30a6a82848
Matt Atkins <matt@yoomee.com>
2012-02-06 17:00:08 +0000

    Render file with message.

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,12 +1,13 @@
 class ApisController < ApplicationController
   def show
     get_venues
-    render :json => {:venues => @venues}
+    #render :json => {}
+    #render :json => {:venues => @venues, :message => {:text => "Hello Ian!", :button_text => "Bonza", :button_hidden => false, :id => 4}}
+    render :file => File.join(Rails.root, "lib", "venues.json")
   end
   
   private
   def get_venues
-    
     @venues = Rails.cache.fetch("venues", :expires_in => 1.minute) do
       #Sheff  "53.3933,-1.4452","53.3625,-1.5123"
       #Austin "30.321, -97.713","30.241, -97.761"
