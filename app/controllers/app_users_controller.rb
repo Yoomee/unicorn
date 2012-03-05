cbb59b25a60f9aeb38f3343830e83a674c0c1283
Matt Atkins <matt@yoomee.com>
2012-03-05 16:00:52 +0000

    Admin specific css.

--- app/controllers/app_users_controller.rb
+++ app/controllers/app_users_controller.rb
@@ -3,6 +3,6 @@ class AppUsersController < ApplicationController
   before_filter :authenticate
 
   def index
-    @app_users = AppUser.paginate(:page => params[:page], :per_page => 200)
+    @app_users = AppUser.order('created_at DESC').paginate(:page => params[:page], :per_page => 50)
   end
 end
