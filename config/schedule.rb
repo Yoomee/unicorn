cd2f8839bab5f8e3f86f65a52bf51f395fd07ce9
Matt Atkins <matt@yoomee.com>
2013-03-06 15:49:11 +0000

    Release the unicorn!

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 # Use this file to easily define all of your cron jobs.
 set :output, File.expand_path(File.dirname(__FILE__) + "/../log/cron.log")
 
-# every 5.minutes do
-#   rake "venues:refresh"
-# end
-# 
-# every 3.minutes do
-#   rake "unicorn:move"
-# end
+every 5.minutes do
+  rake "venues:refresh"
+end
+
+every 3.minutes do
+  rake "unicorn:move"
+end
